75f4486819ae3611cd37b4f9f661705c752443c1
omermemis <66531340+omermemis@users.noreply.github.com>
2021-11-10 11:43:52 +0100

    Delete control-in-networked-vehicles-master directory

--- control-in-networked-vehicles-master/+cmmn/InterfaceController.m
+++
@@ -1,8 +0,0 @@
-classdef (Abstract) InterfaceController < handle
-% InterfaceController Interface for a controller class
-    methods (Abstract) 
-        [u,y] = step(obj,y_measured)
-        setup(obj)
-    end
-end
-
