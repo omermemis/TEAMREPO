ad4c7dc77b40af9f8c4854c88416ddbc672f2a71
Jianye Xu <jianye.xu@rwth-aachen.de>
2021-11-30 21:16:07 +0100

    update_30_11_2021

--- main.m
+++ main.m
@@ -3,12 +3,8 @@ function main(varargin)
 
     vehicle_ids = cell2mat(varargin);
     Ts = 0.4; %[s]
-<<<<<<< HEAD
-    hlc = pmpc.Hlc(Ts,vehicle_ids);
-=======
     hlc = pmpc.HlcIdentification(Ts,vehicle_ids);
 %     hlc = pmpc.Hlc(Ts,vehicle_ids);
->>>>>>> 28efdb3b121e0b1ce94a4ba4f7e6f255f9435d45
 %     hlc = cmpc.Hlc(Ts,vehicle_ids);
 %     hlc = dmpc.Hlc(Ts,vehicle_ids);
 
