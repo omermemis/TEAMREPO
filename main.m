37e9f9f622ed51d01ca8e7c03405902d2da8339e
JIANYE-XU <jianye.xu@rwth-aachen.de>
2022-03-22 18:12:40 +0100

    predictive

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ function main(varargin)
     % MAIN  main function
         
     vehicle_ids = cell2mat(varargin);
-    Ts = 0.1; %[s]
+    Ts = 0.1; % [s] sample time
 %     hlc = pmpc.HlcIdentification(Ts,vehicle_ids);
 %     hlc = pmpc.Hlc(Ts,vehicle_ids);
 %     hlc = cmpc.HlcIdentification(Ts,vehicle_ids);
